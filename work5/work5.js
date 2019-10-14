90d1a2b2e3925148f6f7b30d0d5955215dfc3cd3
HayashiEigo <1634084297@qq.com>
2019-10-14 09:55:52 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,5 +1,14 @@
 //9x9的空表
-
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=9;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table').innerHTML = str
 //倒九九乘法空表
 str = '<table border="1">'
 for(let i=9;i>0;--i){
@@ -13,3 +22,13 @@ str += '</table>'
 document.getElementById('table2').innerHTML = str
 
 //倒九九乘法表
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str +='<td>'+j+'*'+i+'='+(j*i)+'</td>'
+    }
+    str +='</tr>'
+}
+str+='</table>'
+document.getElementById('table3').innerHTML = str
