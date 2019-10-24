f8bb07506feceb51d1e5e8ad350d5f78006cee41
HayashiEigo <1634084297@qq.com>
2019-10-25 00:15:13 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,4 +1,14 @@
-function lin(num) {
+function goldbach(){
+  let odd = parseInt(document.getElementById('odd').value)
+  let lyh = parseFloat(document.getElementById('odd').value)
+ if(isNaN(lyh)||lyh<=2||lyh%2==1){
+   alert('请输入大于2的偶数')
+   return false
+  }else{
+   isTrue(odd)
+  }
+}
+ function lin(num) {
       var sum = 0;
   for (var i = 1; i <= num; i++) {
           if (num % i == 0) {
@@ -11,9 +21,7 @@ function lin(num) {
         return false;
    }
   }
-  function goldbach(odd) {
-    odd = document.getElementById('odd').value
-    odd = parseInt(odd);
+  function isTrue(odd){
     let str = '';
     for (var x = 2; x < odd; x++) {
         var z = odd - x;
@@ -21,9 +29,5 @@ function lin(num) {
             str += ("偶数" + odd + "可以拆分成质数" + x + "和" + z + '<br>')
              }
                      }
-        document.getElementById('goldbach').innerHTML = str;
-        if(isNaN(odd)||odd<=2||odd%2==1){
-          alert('请输入大于2的偶数')
-          return false
-        }
+     document.getElementById('goldbach').innerHTML = str;
       }
